797644fd2546fe1813c6c805367825a4ccc64343
Pierre Burel <contact@pierreburel.com>
2018-08-16 15:43:33 +0200

    Update

--- index.js
+++ index.js
@@ -12,29 +12,24 @@ const defaults = {
 module.exports = postcss.plugin(pluginName, (opts = {}) => (root) => {
   const options = Object.assign({}, defaults, opts);
 
-  const convert = (values, to) => values.replace(/(-?(?:\d+)?(?:\.?\d+)?)(rem|px)/g, (match, value, from) => {
+  const convert = (values, to) => reduceFunctionCall(values, functionName, (values) => values.replace(/(-?(?:\d+)?(?:\.?\d+)?)(rem|px)/g, (match, value, from) => {
     if (from === 'px' && to === 'rem') {
-      return parseFloat(value) / options.baseline + 'rem';
+      return parseFloat(value) / options.baseline + to;
     }
     if (from === 'rem' && to === 'px') {
-      return parseFloat(value) * options.baseline + 'px';
+      return parseFloat(value) * options.baseline + to;
     }
     return match;
-  })
+  }));
 
   root.walkDecls((decl) => {
     if (decl.value && decl.value.includes(functionName + '(')) {
       if (options.fallback && options.convert !== 'px') {
         decl.cloneBefore({
-          value: reduceFunctionCall(decl.value, functionName, (values) => {
-            return convert(values, 'px');
-          })
+          value: convert(decl.value, 'px')
         });
       }
-
-      decl.value = reduceFunctionCall(decl.value, functionName, (values) => {
-        return convert(values, options.convert);
-      });
+      decl.value = convert(decl.value, options.convert);
     }
   });
 });
